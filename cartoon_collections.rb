819d104867b4a464841cd1712fcaaf5cb7aafbca
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-29 23:05:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,15 +8,24 @@ def roll_call_dwarves (array)
   puts "#{array[i]}"
   i +=1 
 end
- 
+
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = []
+  i=0 
+  while i < array.length
+  new_array<< array[i]
+  .capitalize + "i"
+  i +=1 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (array)
+  i = 0
+  if array.any? {|i| i.length > 4}
+    return true
+  else return false
+    i=i+1 
 end
 
 def find_the_cheese# code an argument here
