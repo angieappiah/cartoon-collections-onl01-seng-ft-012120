cd5249a4799e11abf55cf7ff2ef6c124a3fe837c
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-29 23:07:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,7 @@ def long_planeteer_calls (array)
     i=i+1 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks =
 end
